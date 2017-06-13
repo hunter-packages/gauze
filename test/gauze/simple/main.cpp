bc5d26d5635a93cc1303bc8b7cae49b42d88e723
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-06-13 16:09:01 +0300

    Add simple test

---
+++ test/gauze/simple/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream> // std::cout
+#include <cstdlib> // EXIT_SUCCESS
+
+int gauze_main(int argc, char** argv) {
+  std::cout << "Hello, Gauze!" << std::endl;
+  return EXIT_SUCCESS;
+}
