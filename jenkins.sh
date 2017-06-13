0b58be4da34c861bd718fe0982ef63b7c7bdbae5
Ruslan Baratov <ruslan_baratov@yahoo.com>
2017-06-13 18:33:15 +0300

    Add jenkins.sh script

---
+++ jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+set -x
+
+# '--ios-{multiarch,combined}' do nothing for non-iOS builds
+polly.py \
+  --toolchain ${TOOLCHAIN} \
+  --config ${CONFIG} \
+  --verbose \
+  --ios-multiarch --ios-combined \
+  --fwd \
+  HUNTER_USE_CACHE_SERVERS=ONLY \
+  HUNTER_DISABLE_BUILDS=YES \
+  --test \
+  --install
